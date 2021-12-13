7f8474948e8b594b71d56d0f656c61186d327262
eutro <benedek.szilvasy@gmail.com>
2021-12-13 20:24:21 +0000

    Initial commit

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "raylib-2dx")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/racket-raylib-2dx.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(eutro))
+(define license '(Apache-2.0 OR MIT))
